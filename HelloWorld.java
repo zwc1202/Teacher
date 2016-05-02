146f98f6f60580475d523c1546ceae5e04393ff9
zwc1202 <410608647@qq.com>
2016-05-02 14:47:30 +0800

    first

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yysy.class1;
+
+import java.util.Date;
+
+public class HelloWorld
+{
+    public static void main(String[] args){
+	    System.out.println("hello world "+new Date());
+		}
+}
